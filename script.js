b5b941b8b6d6d1fc47c4e46afa4eb3cbf00d1585
Balthazar <balthazar.muhlstein@etu.univ-lyon1.fr>
2025-01-31 14:37:08 +0100

    Button start

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 console.log("hello")
+
+wordList = [
+    "javascript",
+    "golang",
+    "elm",
+    "gns",
+    "java",
+    "react",
+    "angular"
+]
+
+function start(wordList) {
+    console.log("you clicked on the button")
+    console.log()
+}
+
+function createCardWords(wordList) {
+    copy = wordList
+
+}
+
+function shuffle(array) {
+    let currentIndex = array.length;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  }
